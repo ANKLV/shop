0fe1f2e51150596e95992e0d9a4773e679da79db
Andrew_Lux <nefesh.samael@gmail.com>
2021-01-23 04:05:14 +0200

    fix error cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dxe9w9yyw'
+  config.api_key = '153691352153878'
+  config.api_secret = 'nt8uCgCWut7Yd9HanAN7mBUxNjU'
+  config.secure = true
+  config.cdn_subdomain = true
+end
